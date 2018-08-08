750258b63dbf14506859b19fe7661f80dc3d7adf
castle <castleliu40@gmail.com>
2018-08-08 17:26:16 +0800

    add popover

--- js/main.js
+++ js/main.js
@@ -2,11 +2,37 @@ $(document).ready(function () {
     let $charCount, maxCharCount;
 
     $charCount = $('#tweet-modal .char-count');
-    maxCharCount = parseInt($charCount.data('max'),10);
+    maxCharCount = parseInt($charCount.data('max'), 10);
 
-    $('#tweet-modal textarea').on('keyup',function (e) {
+    $('#tweet-modal textarea').on('keyup', function (e) {
         let tweetLength = $(e.currentTarget).val().length;
 
         $charCount.html(maxCharCount - tweetLength);
-    })
+    });
+
+    $('[data-toggle="tooltip"]').tooltip({animation: true});
+    let popoverContentTemplate = '' +
+        '<img src="imgs/breed.jpg" class="rounded-circle">' +
+        '<div class="info">' +
+        '<strong>Dog Breeds</strong>' +
+        '<a href="#" class="btn btn-default">' +
+        '<i class="fa fa-plus" aria-hidden="true"></i>' + 'Follow' + '</a>' + '</div>';
+
+    let popovers = $('[data-toggle="popover"]');
+
+    popovers.popover({
+        placement: 'bottom',
+        html: true,
+        content: function () {
+            return popoverContentTemplate;
+        }
+    });
+
+    popovers.on('show.bs.popover', function () {
+        let $icon = $(this).find('i.fa');
+
+        $icon.removeClass('fa-plus').addClass('fa-check');
+        $(this).html($icon)
+            .append(" Following");
+    });
 });
